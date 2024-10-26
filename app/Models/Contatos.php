0922033876221bb6e24a65d26df5a38618fab633
Mauro Shindi Miyauti <msmiyauti@gmail.com>
2024-10-26 10:16:24 -0300

    seeders

--- app/Models/Contatos.php
+++ app/Models/Contatos.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Database\Factories\ContatosFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Contatos extends Model
 {
-    //
+
+    use HasFactory;
+    
+    protected static function newFactory(): Factory
+    {
+        return ContatosFactory::new();
+    }
 }
--- database/seeders/ContatosSeeder.php
+++ database/seeders/ContatosSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Contatos;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,130 @@ class ContatosSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Contatos::factory()->create(
+            [
+                'nome' => "Mauro Miyauti",
+                'cpf' => "043.152.039-98",
+                'email' => "msmiyauti@gmail.com",
+                'telefone' => "44991590377",
+                'logradouro' => "Rua Weslley Cesar Vanzo",
+                'numero' => "189",
+                'complemento'=> "AP 00",
+                'bairro'=> "Fazenda Gleba Palhano",
+                'cidade' => "Londrina",
+                'uf' => "PR", 
+                'cep' => "86050500",
+                'latitude' => "-23.326452152480098",
+                'longitude' => "-51.18034835950851",
+            ]);
+        Contatos::factory()->create( 
+            [
+                'nome' => fake()->name(),
+                'cpf' => "794.865.630-80",
+                'email' => fake()->unique()->safeEmail(),
+                'telefone' => fake()->phoneNumber(),
+                'logradouro' => fake()->streetName(),
+                'numero' => fake()->buildingNumber(),
+                'complemento'=> "AP ". fake()->numberBetween(10,1000),
+                'bairro'=> "Centro",
+                'cidade' => fake()->city(),
+                'uf' => "PR", 
+                'cep' => fake()->postcode(),
+                'latitude' => fake()->latitude(),
+                'longitude' => fake()->longitude(),
+            ]);
+        Contatos::factory()->create([
+                'nome' => fake()->name(),
+                'cpf' => "763.168.010-84",
+                'email' => fake()->unique()->safeEmail(),
+                'telefone' => fake()->phoneNumber(),
+                'logradouro' => fake()->streetName(),
+                'numero' => fake()->buildingNumber(),
+                'complemento'=> "AP ". fake()->numberBetween(10,1000),
+                'bairro'=> "Centro",
+                'cidade' => fake()->city(),
+                'uf' => "PR", 
+                'cep' => fake()->postcode(),
+                'latitude' => fake()->latitude(),
+                'longitude' => fake()->longitude(),
+        ]);
+        Contatos::factory()->create([
+                'nome' => fake()->name(),
+                'cpf' => "761.334.690-05",
+                'email' => fake()->unique()->safeEmail(),
+                'telefone' => fake()->phoneNumber(),
+                'logradouro' => fake()->streetName(),
+                'numero' => fake()->buildingNumber(),
+                'complemento'=> "AP ". fake()->numberBetween(10,1000),
+                'bairro'=> "Centro",
+                'cidade' => fake()->city(),
+                'uf' => "PR", 
+                'cep' => fake()->postcode(),
+                'latitude' => fake()->latitude(),
+                'longitude' => fake()->longitude(),
+        ]);
+        Contatos::factory()->create([
+                'nome' => fake()->name(),
+                'cpf' => "744.502.350-02",
+                'email' => fake()->unique()->safeEmail(),
+                'telefone' => fake()->phoneNumber(),
+                'logradouro' => fake()->streetName(),
+                'numero' => fake()->buildingNumber(),
+                'complemento'=> "AP ". fake()->numberBetween(10,1000),
+                'bairro'=> "Centro",
+                'cidade' => fake()->city(),
+                'uf' => "PR", 
+                'cep' => fake()->postcode(),
+                'latitude' => fake()->latitude(),
+                'longitude' => fake()->longitude(),
+        ]);
+        Contatos::factory()->create([
+                'nome' => fake()->name(),
+                'cpf' => "756.557.980-77",
+                'email' => fake()->unique()->safeEmail(),
+                'telefone' => fake()->phoneNumber(),
+                'logradouro' => fake()->streetName(),
+                'numero' => fake()->buildingNumber(),
+                'complemento'=> "AP ". fake()->numberBetween(10,1000),
+                'bairro'=> "Centro",
+                'cidade' => fake()->city(),
+                'uf' => "PR", 
+                'cep' => fake()->postcode(),
+                'latitude' => fake()->latitude(),
+                'longitude' => fake()->longitude(),
+        ]);
+        Contatos::factory()->create([
+                'nome' => fake()->name(),
+                'cpf' => "664.965.680-14",
+                'email' => fake()->unique()->safeEmail(),
+                'telefone' => fake()->phoneNumber(),
+                'logradouro' => fake()->streetName(),
+                'numero' => fake()->buildingNumber(),
+                'complemento'=> "AP ". fake()->numberBetween(10,1000),
+                'bairro'=> "Centro",
+                'cidade' => fake()->city(),
+                'uf' => "PR", 
+                'cep' => fake()->postcode(),
+                'latitude' => fake()->latitude(),
+                'longitude' => fake()->longitude(),
+        ]);
+        Contatos::factory()->create([
+                'nome' => fake()->name(),
+                'cpf' => "548.713.420-00",
+                'email' => fake()->unique()->safeEmail(),
+                'telefone' => fake()->phoneNumber(),
+                'logradouro' => fake()->streetName(),
+                'numero' => fake()->buildingNumber(),
+                'complemento'=> "AP ". fake()->numberBetween(10,1000),
+                'bairro'=> "Centro",
+                'cidade' => fake()->city(),
+                'uf' => "PR", 
+                'cep' => fake()->postcode(),
+                'latitude' => fake()->latitude(),
+                'longitude' => fake()->longitude(),
+            ]
+        );
+
+        
     }
 }
